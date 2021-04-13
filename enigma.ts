cd4b3f72ae614b70ce6a2b84de48f91d0267b6d8
awphi <jellymoos3@gmail.com>
2021-04-13 23:29:48 +0100

    initial commit

---
+++ enigma.ts
@@ -0,0 +1,15 @@
+class Rotor {
+  private static alphabet: string[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  private wiring: string[] = null;
+  private position: number = 0;
+
+  constructor(wiring) {
+    if (!Array.isArray(wiring)) {
+      wiring = wiring.split("");
+    }
+
+    this.wiring = wiring;
+  }
+
+  map(chr) {}
+}
